11e661f76cc207909d3c81955a412b7453cfbbe5
ShuvroSankar <sankarshuvro1010@gmail.com>
2023-07-25 03:13:55 +0600

    add delete and edit functionality to listing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,12 +8,14 @@ import {
   doc,
   collection,
   query,
+  deleteDoc,
   where,
   orderBy,
   getDocs,
 } from "firebase/firestore";
 import { db } from "../firebase";
 import { FcHome } from "react-icons/fc";
+
 export default function Profile() {
   const auth = getAuth();
   const naviagte = useNavigate();
@@ -76,7 +78,19 @@ export default function Profile() {
     }
     fetchUserListings();
   }, [auth.currentUser.uid]);
-
+  async function onDelete(listingID) {
+    if (window.confirm("Are you sure you want to delete")) {
+      await deleteDoc(doc(db, "listings", listingID));
+      const updateListings = listings.filter(
+        (listing) => listing.id !== listingID
+      );
+      setListings(updateListings);
+      toast.success("Successfully deleted listing");
+    }
+  }
+  function onEdit(listingID) {
+    naviagte(`/edit-listing/${listingID}`);
+  }
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col ">
@@ -149,6 +163,8 @@ export default function Profile() {
                   key={listing.id}
                   id={listing.id}
                   listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
